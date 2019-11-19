0344f8d685f8c13438696a5994430955bc488b08
Hubert Yang <offspring7890@yahoo.com>
2019-11-19 10:35:21 -0800

    gethead service and router completed

--- test/setup.js
+++ test/setup.js
@@ -5,8 +5,8 @@ process.env.JWT_EXPIRY = '3m'
 
 require('dotenv').config()
 
-process.env.TEST_DB_URL = "postgresql://dunder_mifflin@localhost/spaced-repetition-test"
-  || "postgresql://dunder_mifflin@localhost/spaced-repetition-test"
+process.env.TEST_DB_URL = "postgresql://postgres@localhost/spanishmedterms"
+  || "postgresql://postgres@localhost/spanishmedterms-test"
 
 const { expect } = require('chai')
 const supertest = require('supertest')
